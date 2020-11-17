12fa28836102820b795390550eda02865fabea4b
happylynx <happylynx>
2020-11-18 00:43:55 +0100

    common.js removed

--- signInToYouTubeClicker.js
+++ signInToYouTubeClicker.js
@@ -4,6 +4,7 @@ main()
 async function main() {
     console.log("from main")
     await whenLoaded()
+    console.log('loaded')
     let button = findIAgreeButton()
     if (button) {
         resolveBeforeYouContinueDialog(button)
@@ -17,9 +18,6 @@ async function main() {
 function findIAgreeButton() {
     // html body ytd-app ytd-popup-container.style-scope.ytd-app paper-dialog.style-scope.ytd-popup-container yt-upsell-dialog-renderer.style-scope.ytd-popup-container div#dialog.style-scope.yt-upsell-dialog-renderer div#button-container.style-scope.yt-upsell-dialog-renderer div#dismiss-button.style-scope.yt-upsell-dialog-renderer yt-button-renderer.style-scope.yt-upsell-dialog-renderer.style-text.size-small a.yt-simple-endpoint.style-scope.yt-button-renderer paper-button#button.style-scope.yt-button-renderer.style-text.size-small yt-formatted-string#text.style-scope.yt-button-renderer.style-text.size-small
     let button = document.querySelector('#button')
-    if (!button) {
-        debugger
-    }
     return button
 }
 
@@ -28,5 +26,23 @@ function findIAgreeButton() {
  */
 function resolveBeforeYouContinueDialog(button) {
     button.click()
+}
+
+/**
+ * Always completes, never rejects.
+ * @returns {Promise<undefined>} when the document is loaded
+ */
+function whenLoaded() {
+    console.log('whenLoaded', document.readyState)
+    if (document.readyState == 'complete') {
+        return Promise.resolve()
+    }
+    return new Promise((complete, reject) => {
+        document.addEventListener('load', (loadEvent) => {
+            console.log('loadEvent', loadEvent)
+            complete()
+        })
+        console.log('load listener added')
+    })
 }
 
