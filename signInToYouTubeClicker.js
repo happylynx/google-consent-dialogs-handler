e53de4af146b236701a1221b5c3fdb3ec58c5b3c
happylynx <happylynx>
2020-11-18 02:28:09 +0100

    youtube clicker done

--- signInToYouTubeClicker.js
+++ signInToYouTubeClicker.js
@@ -7,9 +7,29 @@ async function main() {
     console.log('loaded')
     let button = findIAgreeButton()
     if (button) {
-        resolveBeforeYouContinueDialog(button)
+        resolveDialog(button)
         return
     }
+    addMutationObserver()
+}
+
+function addMutationObserver() {
+    const observedElement = document.querySelector('ytd-app')
+    const config = {
+        attributes: false,
+        childList: true, 
+        subtree: false
+    }
+    function mutationCallback(mutationList, observer) {
+        const button = findIAgreeButton()
+        if (!button) {
+            return
+        }
+        observer.disconnect()
+        resolveDialog(button)
+    }
+    const observer = new MutationObserver(mutationCallback)
+    observer.observe(observedElement, config)
 }
 
 /**
@@ -17,14 +37,14 @@ async function main() {
  */
 function findIAgreeButton() {
     // html body ytd-app ytd-popup-container.style-scope.ytd-app paper-dialog.style-scope.ytd-popup-container yt-upsell-dialog-renderer.style-scope.ytd-popup-container div#dialog.style-scope.yt-upsell-dialog-renderer div#button-container.style-scope.yt-upsell-dialog-renderer div#dismiss-button.style-scope.yt-upsell-dialog-renderer yt-button-renderer.style-scope.yt-upsell-dialog-renderer.style-text.size-small a.yt-simple-endpoint.style-scope.yt-button-renderer paper-button#button.style-scope.yt-button-renderer.style-text.size-small yt-formatted-string#text.style-scope.yt-button-renderer.style-text.size-small
-    let button = document.querySelector('#button')
+    let button = document.querySelector('yt-upsell-dialog-renderer #button:not([class~=style-suggestive])')
     return button
 }
 
 /**
  * @param {HTMLElementPrototype} button 
  */
-function resolveBeforeYouContinueDialog(button) {
+function resolveDialog(button) {
     button.click()
 }
 
@@ -33,16 +53,9 @@ function resolveBeforeYouContinueDialog(button) {
  * @returns {Promise<undefined>} when the document is loaded
  */
 function whenLoaded() {
-    console.log('whenLoaded', document.readyState)
     if (document.readyState == 'complete') {
         return Promise.resolve()
     }
-    return new Promise((complete, reject) => {
-        document.addEventListener('load', (loadEvent) => {
-            console.log('loadEvent', loadEvent)
-            complete()
-        })
-        console.log('load listener added')
-    })
+    console.warn('Ups. This still needs some love.')
 }
 
