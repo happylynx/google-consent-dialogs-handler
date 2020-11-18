c6008783fb6229cf27524c96fb0fafc8d6b3274d
happylynx <happylynx>
2020-11-18 03:27:50 +0100

    windows packaging script + .gitignore

---
+++ createZip.ps1
@@ -0,0 +1,4 @@
+Compress-Archive `
+  -Path page-scripts,icons,beforeYouContinueClicker.js,manifest.json,signInToYouTubeClicker.js `
+  -DestinationPath google-consent-dialogs-handler.zip `
+  -Force
