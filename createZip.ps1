1c11bb605e1a715824c9fb2fdc9acb62494853c5
happylynx <happylynx>
2020-11-18 14:34:38 +0100

    powershell execution comments

--- createZip.ps1
+++ createZip.ps1
@@ -1,3 +1,5 @@
+# enable powershell execution (https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_execution_policies?view=powershell-7.1)
+# Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope process
 Compress-Archive `
   -Path page-scripts,icons,beforeYouContinueClicker.js,manifest.json,signInToYouTubeClicker.js `
   -DestinationPath google-consent-dialogs-handler.zip `
