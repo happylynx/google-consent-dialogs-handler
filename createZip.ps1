87fb122610c5790a313a5724636f60c65912065b
happylynx <happylynx>
2020-11-21 01:12:01 +0100

    common.js added to zipping powershell script

--- createZip.ps1
+++ createZip.ps1
@@ -1,6 +1,6 @@
 # enable powershell execution (https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_execution_policies?view=powershell-7.1)
 # Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope process
 Compress-Archive `
-  -Path page-scripts,icons,beforeYouContinueClicker.js,manifest.json,signInToYouTubeClicker.js `
+  -Path page-scripts,icons,beforeYouContinueClicker.js,manifest.json,signInToYouTubeClicker.js,common.js `
   -DestinationPath google-consent-dialogs-handler.zip `
   -Force
